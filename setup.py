6a37b48f7c2cf6665c04ae39a81585d69fe257c5
Rogelio Pedroza Duarte <rogerboy38@hotmail.com>
2025-07-27 08:25:23 -0600

    Update setup.py
    logs update

--- setup.py
+++ setup.py
@@ -59,3 +59,12 @@ setup(
     keywords="frappe erpnext aloe vera distribution r&d regulatory compliance",
     python_requires=">=3.8",
 )
+# Add setup.py to each repository's root
+echo "from setuptools import setup, find_packages
+setup(
+    name='repo_name',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['frappe']
+)" > setup.py
